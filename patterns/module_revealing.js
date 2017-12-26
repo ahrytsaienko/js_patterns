261f7ae36c55d4d190126955942203c3644a7f49
temsan89 <temsan89@gmail.com>
2017-12-26 22:12:16 +0200

    module-revealing

--- patterns/module_revealing.js
+++ patterns/module_revealing.js
@@ -1,10 +1,57 @@
-// STANDARD MODULE PATTERN
+//*** STANDARD MODULE PATTERN ***
 
-const UICtrl = (function(){
-    let text = 'John Doe';
+const UICtrl = (function () {
+    //private stuff
+    let _name = 'Kristina Doe';
 
-    const changeName = function(){
-       const element = document.querySelector('h1');
-        
+    const changeName = function (newName) {
+        const element = document.querySelector('h1');
+        _name = newName;
+        element.textContent = _name;
+
     }
+
+    return {
+        callChangeName: function (newName) {
+            changeName(newName);
+            // console.log(_name);
+        }
+    }
+
 })();
+
+//UICtrl.callChangeName('Brad');
+
+//*** REVEALING MODULE PATTERN ***
+
+const ItemCtrl = (function () {
+    let _data = [];
+
+    function add(item) {
+        _data.push(item);
+        console.log(`Item ${item.id} - ${item.name} add...`)
+    }
+
+    function get(id) {
+        return _data.find(item => {
+            return item.id === id;
+        });
+    }
+
+    return {
+        add,
+        get
+    }
+
+})();
+
+// ItemCtrl.add({id: 1, name: 'John'})
+// console.log(ItemCtrl.get(1))
+
+
+
+
+
+
+
+
