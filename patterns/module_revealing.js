f17e8cc439e1b3770a58c373d70e24340a1b4787
temsan89 <temsan89@gmail.com>
2017-12-26 19:54:09 +0200

    init 1 pattern

---
+++ patterns/module_revealing.js
@@ -0,0 +1,10 @@
+// STANDARD MODULE PATTERN
+
+const UICtrl = (function(){
+    let text = 'John Doe';
+
+    const changeName = function(){
+       const element = document.querySelector('h1');
+        
+    }
+})();
